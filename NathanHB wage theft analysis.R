76e511e7d830e6253b8f5a02ad986de7ef4ba360
“nathanneuro” <nathan.helm.burger@gmail.com>
2016-08-13 16:37:06 -0700

    working on R script

--- NathanHB wage theft analysis.R
+++ NathanHB wage theft analysis.R
@@ -17,11 +17,20 @@
 # naic_cd2 ,3 ,4
 
 
-# join zipcodes to counties to FIPS (FIPS is ultimate goal, b/c )
+# join zipcodes to counties to FIPS (FIPS is ultimate goal, b/c geolocation best w FIPS)
 
-# egregiouness - total size or total per emp
-
+# egregiousness - total size or total per emp
 # vulnerability - min wage focused, low wage counties
 
 # Questions: Industries (at each naic level 2,3,4,5) ranked by:
-# max
+# 1. total back wages (egregiousness focus)
+# 2. back wages per worker
+# 3. total minimum-wage-violation back wages (vulnerability focus)
+# 4. min-wage-bw per worker
+
+# Note: check for outliers, present the data both with and without
+
+# 5. Check for correlations with demographic data
+# 6. Find unusual data points (high or low) to help find possible trouble spots
+# 7. Save results in a format which can be managed and added to consistently going forward into the future (perhaps an online SQL database)
+
